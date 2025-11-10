45af6acc789d2c6ebd5b36ebb99b8a6d682ffb08
DAVID ANTONIO PALMA FLORES <156796608+palmasd@users.noreply.github.com>
2025-11-10 23:55:49 +0100

    Delete ProyectoJDBC directory

--- ProyectoJDBC/src/Main.java
+++
@@ -1,7 +0,0 @@
-import controller.ControladorPreguntas;
-
-public class Main {
-    public static void main(String[] args) {
-        ControladorPreguntas.iniciar();
-    }
-}
--- ProyectoJDBC/src/repositorio/RepositorioBD.java
+++
@@ -1,7 +0,0 @@
-package repositorio;
-
-public class RepositorioBD {
-
-
-
-}
